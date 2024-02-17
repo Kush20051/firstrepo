d2a89121a38a6b502953bd0d5e062d1c93fadf00
Kush Agarwal <2023KUCP1034@gmail.com>
2024-02-17 18:01:23 +0530

    fourthcommit

---
+++ twonumbers.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    int b;
+    printf("Enter two numbers");
+    scanf("%d%d",&a,&b);
+    int c=a+b;
+    printf("The sum of two numbers is equal to %d",c);
+}
